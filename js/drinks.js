ffca4d959660f4c328f82acc951b217dd240b716
Hemstreet <jonhemstreet@gmail.com>
2017-12-07 19:56:31 -0700

    v1.uhoh

---
+++ js/drinks.js
@@ -0,0 +1,87 @@
+/*
+0: pineapple Juice
+1: cranberry
+2: Margarita mix
+3: Orange juice
+4: tequila
+5: whiskey
+6: vodka
+7: coke
+*/
+var pumps = {
+  pineapple: 0,
+  cranberry: 1,
+  margarita: 2,
+  orange: 3,
+  tequila: 4,
+  whiskey: 5,
+  vodka: 6,
+  coke: 7
+};
+
+var isPouring = false;
+
+function pour(pump, oz) {
+
+  $.get('/' + pump + '/' + oz, function( data ) {
+    console.log(pump, oz);
+  });
+}
+
+function pourScrewdriver() {
+	return $.when(
+		pour(pumps['vodka'], 1.5),
+		pour(pumps['orange'], 4)
+	       );
+}
+
+function pourVodkaCranberry() {
+	return $.when(
+	         pour(pumps['vodka'], 1),
+		 pour(pumps['cranberry'], 4.5)
+	       )
+}
+
+function pourTequilaSunrise() {
+	return $.when(
+	         pour(pumps['tequila'], 1.5),
+		 pour(pumps['orange'], 4)
+ 	       )
+}
+
+function pourBayBreeze() {
+	return $.when(
+	         pour(pumps['vodka'], 2),
+		 pour(pumps['cranberry'], 3),
+		 pour(pumps['pineapple'], 3)
+ 	       )
+}
+
+function pourJackAndCoke() {
+	return $.when(
+	       	pour(pumps['whiskey'], 2),
+		pour(pumps['coke'], 10)
+	       )
+}
+
+function pourMargarita() {
+	return $.when(
+	   	pour(pumps['tequila'], 1),
+		pour(pumps['margarita'], 3)
+	       )
+}
+
+function pourShot(type) {
+	console.log('pouring', type);
+    return $.when(
+        pour(pumps[type], 1)
+    )
+}
+
+function isBusy() {
+	if(isPouring) {
+		console.log('is Pouring');
+	}
+
+	return isPouring;
+}
---
+++ script.js
@@ -0,0 +1,47 @@
+var gpio = require('pi-gpio');
+var path = require('path');
+const express = require('express');
+const app = express();
+
+var ozDuration = 900;
+var initialState = 0;
+var port = 3000;
+
+var pins = [40, 38, 36, 37, 35, 33, 31, 29];
+
+function pour(pin, ounces) {
+
+    var oz = ounces * ozDuration;
+
+    gpio.open(pin, "output", function(err) {
+    	gpio.write(pin, initialState, function() {
+    		setTimeout(function() {
+    			gpio.write(pin, !initialState, function() {
+    				gpio.close(pin)
+    			});
+    		}, oz) ;
+    	});
+    });
+}
+
+app.use('/scripts', express.static('js'));
+app.use('/css', express.static('css'));
+app.use('/media', express.static('media'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/credits', (req, res) => {
+    res.sendFile(path.join(__dirname + '/credits.html'));
+});
+
+app.get('/:pin/:amount', (req, res) => {
+    let params = req.params,
+        pin = params.pin,
+        amount = params.amount;
+    pour(pins[pin], amount);
+    res.send(`Pouring ${pin} of ${amount}`);
+});
+
+app.listen(port, () => console.log(`Port ${port}!`));
